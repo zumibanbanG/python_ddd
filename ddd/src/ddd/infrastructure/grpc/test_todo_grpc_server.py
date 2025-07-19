34667708026ac7986b89445087eb6c9ce00b3a11
鈴木直澄 <naozumi@ZumiMac.local>
2025-07-19 14:44:38 +0900

    ciで怒られた

--- ddd/src/ddd/infrastructure/grpc/test_todo_grpc_server.py
+++ ddd/src/ddd/infrastructure/grpc/test_todo_grpc_server.py
@@ -1,18 +1,17 @@
 import grpc
-from pb import todo_pb2
-from pb import todo_pb2_grpc
 
-def run():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = todo_pb2_grpc.TasksStub(channel)
+from pb import todo_pb2, todo_pb2_grpc
+
+
+def run() -> None:
+    channel = grpc.insecure_channel("localhost:50051")
+    stub = todo_pb2_grpc.TasksStub(channel)  # type: ignore
     request = todo_pb2.CreateTaskRequest(
-        id="1",
-        title="テストタスク",
-        status="todo",
-        due_date="2025-07-20"
+        id="1", title="テストタスク", status="todo", due_date="2025-07-20"
     )
     response = stub.CreateTask(request)
     print(f"Response: id={response.id}")
+
 
 if __name__ == "__main__":
     run()

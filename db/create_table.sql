c532ae5f68120e8c97583dc092028da81b534a84
鈴木直澄 <naozumi@ZumiMac.local>
2025-07-21 13:41:34 +0900

    DBの修正

--- db/create_table.sql
+++ db/create_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tasks (
-    id VARCHAR(36) PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     status VARCHAR(16) NOT NULL,
     due_date DATETIME
